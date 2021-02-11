a89e75fe697adf84a6221d78fd017781b0d38acb
Venky <venkatramanm@gmail.com>
2021-02-11 22:07:25 +0530

    UserAgent Size increased

--- src/main/java/com/venky/swf/plugins/mobilesignup/db/model/UserLogin.java
+++ src/main/java/com/venky/swf/plugins/mobilesignup/db/model/UserLogin.java
@@ -1,6 +1,7 @@
 package com.venky.swf.plugins.mobilesignup.db.model;
 
 import com.venky.geo.GeoLocation;
+import com.venky.swf.db.annotations.column.COLUMN_SIZE;
 import com.venky.swf.db.model.Model;
 
 import java.sql.Timestamp;
@@ -9,6 +10,7 @@ public interface UserLogin extends Model , GeoLocation {
     public long getUserId();
     public void setUserId(long id);
 
+    @COLUMN_SIZE(512)
     public String getUserAgent();
     public void setUserAgent(String agent);
 
