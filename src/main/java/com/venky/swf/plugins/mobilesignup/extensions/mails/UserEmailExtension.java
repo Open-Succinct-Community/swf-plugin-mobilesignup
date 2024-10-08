c760ca9ad2a90882068d5528af7482ccd9cd3426
Venky <venkatramanm@gmail.com>
2024-10-08 18:41:04 +0530

    Moved send email to after create

--- src/main/java/com/venky/swf/plugins/mobilesignup/extensions/mails/UserEmailExtension.java
+++ src/main/java/com/venky/swf/plugins/mobilesignup/extensions/mails/UserEmailExtension.java
@@ -2,11 +2,8 @@ package com.venky.swf.plugins.mobilesignup.extensions.mails;
 
 import com.venky.core.util.ObjectUtil;
 import com.venky.swf.db.extensions.BeforeModelSaveExtension;
-import com.venky.swf.plugins.background.core.TaskManager;
-import com.venky.swf.plugins.collab.agents.SendOtp;
-import com.venky.swf.plugins.collab.db.model.user.Phone;
+import com.venky.swf.db.extensions.ModelOperationExtension;
 import com.venky.swf.plugins.collab.db.model.user.UserEmail;
-import com.venky.swf.plugins.collab.db.model.user.UserPhone;
 import com.venky.swf.plugins.mobilesignup.db.model.SignUp;
 import com.venky.swf.plugins.mobilesignup.db.model.User;
 import com.venky.swf.sql.Conjunction;
@@ -16,11 +13,12 @@ import com.venky.swf.sql.Select;
 
 import java.util.List;
 
-public class BeforeSaveUserEmail extends BeforeModelSaveExtension<UserEmail> {
+public class UserEmailExtension extends ModelOperationExtension<UserEmail> {
 
     static {
-        registerExtension(new BeforeSaveUserEmail());
+        registerExtension(new UserEmailExtension());
     }
+
 
 
 
@@ -35,16 +33,6 @@ public class BeforeSaveUserEmail extends BeforeModelSaveExtension<UserEmail> {
                         && signUp.isValidated());
             }
         }
-        boolean isUserSignedUp = !model.getUser().getRawRecord().getAsProxy(User.class).getSignUps().isEmpty();
-
-        if (!model.isValidated() && ObjectUtil.isVoid(model.getLastOtp()) ){
-            if (isUserSignedUp) {
-                TaskManager.instance().executeAsync(new SendOtp(model), false);
-            }else {
-                model.setValidated(true);
-            }
-        }
-        
         if (model.isValidated() && model.getRawRecord().isFieldDirty("VALIDATED")){
             Expression expression = new Expression(getPool(), Conjunction.AND);
             expression.add(new Expression(getPool(), "email", Operator.EQ, model.getEmail()));
@@ -73,6 +61,13 @@ public class BeforeSaveUserEmail extends BeforeModelSaveExtension<UserEmail> {
                 signUp.save(); //Pretend to signp with this email also
             }
 
+        }
+    }
+    @Override
+    protected void afterCreate(UserEmail instance) {
+        super.afterCreate(instance);
+        if (!instance.isValidated()) {
+            instance.sendOtp();
         }
     }
 }
